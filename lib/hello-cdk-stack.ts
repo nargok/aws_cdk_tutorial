6b379abf0c8c4078f9af0e06bb4088482d6ae9fb
nargok <nargok@gmail.com>
2020-09-08 11:56:39 +0900

    create lambda stack

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,18 +1,14 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_10_X, // execution environment
+      code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "hello.handler", // file is "hello", function is "handler"
     });
-
-    const topic = new sns.Topic(this, 'HelloCdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
