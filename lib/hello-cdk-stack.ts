82a881b83e9cd2de13273fcc81200eb82d0ce19e
nargok <nargok@gmail.com>
2020-09-08 12:26:14 +0900

    define api gateway

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -9,6 +10,10 @@ export class HelloCdkStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_10_X, // execution environment
       code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
       handler: "hello.handler", // file is "hello", function is "handler"
+    });
+
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: hello,
     });
   }
 }
